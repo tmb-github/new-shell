e7af06d31908821a6dbf090c262a3a4d31adf528
tmb-github <brodhead.tom@gmail.com>
2022-11-02 13:58:05 -0500

    Created version number toggle in next.config.js that allows easy switching between versions 12 and 13. When using version 13, however, you MUST rename the pages folder as pages-old or similar, otherwise an error is thrown and the app fails.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 
+// Toggle between versions:
+const nextJsVersion = 13;
+
 //const nextSafe = require("next-safe");
 //const isDev = process.env.NODE_ENV !== "production";
 
@@ -14,51 +17,39 @@ const nonceGenerator = () => {
 
 const generatedNonce = nonceGenerator();
 
-//console.info(generatedNonce);
-
-// Next.js 12:
-// script-src 'unsafe-inline' 'self' http: https: 'unsafe-eval' 'nonce-${generatedNonce}' 'strict-dynamic'
-// Next.js 13:
-// script-src 'unsafe-inline' 'self' http: https: 'unsafe-eval'
+// CSP settings:
+const appDirBoolean = nextJsVersion === 13;
+const baseUri = `base-uri 'self'`;
+const connectSrc = `connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://ipapi.co https://httpstat.us https://*.ecwid.com https://ecomm.events/register`;
+const defaultSrc = `default-src 'self'`;
+const fontSrc = `font-src 'self' data: https://fonts.gstatic.com https://cdnjs.cloudflare.com https://*.cloudfront.net https://*.shopsettings.com`;
+const frameAncestors = `frame-ancestors 'self'`;
+const frameSrc = `frame-src *`;
+const imgSrc = `img-src 'self' data: www.google-analytics.com www.googletagmanager.com *.cdninstagram.com *.fbcdn.net *.pinimg.com *.ytimg.com https://*.cloudfront.net`;
+const objectSrc = `object-src 'none'`;
+const manifestSrc = `manifest-src 'self'`;
+const scriptSrc =
+  nextJsVersion === 13
+    ? `script-src 'unsafe-inline' 'self' http: https: 'unsafe-eval'`
+    : `script-src 'unsafe-eval' 'nonce-${generatedNonce}' 'strict-dynamic'`;
+const styleSrc = `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.cloudfront.net`;
+const workerSrc = `worker-src 'self' www.google.com www.youtube.com blob:`;
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: { appDir: true },
+  experimental: { appDir: appDirBoolean },
   compress: true,
   /* Disable x-powered-by header, which exposes next.js as the server */
   poweredByHeader: false,
   env: {
     generatedNonce: `${generatedNonce}`,
+    nextJsVersion: nextJsVersion,
   },
 
   // SEE: https://github.com/UnlyEd/next-right-now/blob/v2-mst-aptd-at-lcz-sty/next.config.js#L112-L215
   async headers() {
     const headers = [
-      {
-        // Make all fonts immutable and cached for one year
-        source: "/static/fonts/(.*?)",
-        headers: [
-          {
-            key: "Cache-Control",
-            // See https://www.keycdn.com/blog/cache-control-immutable#what-is-cache-control-immutable
-            // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control#browser_compatibility
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        // Make all other static assets immutable and cached for one hour
-        source: "/static/(.*?)",
-        headers: [
-          {
-            key: "Cache-Control",
-            // See https://www.keycdn.com/blog/cache-control-immutable#what-is-cache-control-immutable
-            // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control#browser_compatibility
-            value: "public, max-age=3600, immutable",
-          },
-        ],
-      },
       {
         source: "/(.*?)", // Match all paths, including "/" - See https://github.com/vercel/next.js/discussions/17991#discussioncomment-112028
         headers: [
@@ -81,7 +72,7 @@ const nextConfig = {
           { key: "csp12", value: `'nonce-${generatedNonce}' 'strict-dynamic'` },
           {
             key: "content-security-policy",
-            value: `script-src 'unsafe-inline' 'self' http: https: 'unsafe-eval' ; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.cloudfront.net ; img-src 'self' data: www.google-analytics.com www.googletagmanager.com *.cdninstagram.com *.fbcdn.net *.pinimg.com *.ytimg.com https://*.cloudfront.net ; worker-src 'self' www.google.com www.youtube.com blob: ; frame-src * ; connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://ipapi.co https://httpstat.us https://*.ecwid.com https://ecomm.events/register ; font-src 'self' data: https://fonts.gstatic.com https://cdnjs.cloudflare.com https://*.cloudfront.net https://*.shopsettings.com ; object-src 'none' ; base-uri 'self' ; frame-ancestors 'self' ; manifest-src 'self' ; default-src 'self' `,
+            value: `${baseUri} ; ${connectSrc} ; ${defaultSrc} ; ${fontSrc} ; ${frameAncestors} ; ${frameSrc} ; ${imgSrc} ; ${manifestSrc} ; ${objectSrc} ; ${scriptSrc} ; ${styleSrc} ; ${workerSrc} `,
           } /* DO NOT SEND...ALLOW DEFAULT: {
             key: "Content-Type",
             value: "text/html; charset=utf-8",
@@ -111,6 +102,30 @@ const nextConfig = {
             key: "x-xss-protection",
             value: "1; mode=block",
           },*/
+        ],
+      },
+      {
+        // Make all fonts immutable and cached for one year
+        source: "/static/fonts/(.*?)",
+        headers: [
+          {
+            key: "Cache-Control",
+            // See https://www.keycdn.com/blog/cache-control-immutable#what-is-cache-control-immutable
+            // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control#browser_compatibility
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        // Make all other static assets immutable and cached for one hour
+        source: "/static/(.*?)",
+        headers: [
+          {
+            key: "Cache-Control",
+            // See https://www.keycdn.com/blog/cache-control-immutable#what-is-cache-control-immutable
+            // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control#browser_compatibility
+            value: "public, max-age=3600, immutable",
+          },
         ],
       },
     ];
