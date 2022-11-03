015ab5a6ebfc6a3949942a4a15437c8d1e26693a
tmb-github <brodhead.tom@gmail.com>
2022-11-03 16:17:51 -0500

    initial work

--- app/dummy1/page.js
+++ app/dummy1/page.js
@@ -20,6 +20,50 @@ export default function Dummy1() {
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
+        <script
+          type="application/ld+json"
+          id="breadcrumb-list"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "BreadcrumbList",
+              description: "Breadcrumbs list",
+              name: "Breadcrumbs",
+              itemListElement: [
+                {
+                  "@type": "ListItem",
+                  position: 1,
+                  item: {
+                    "@type": "Thing",
+                    "@id": "https://localhost/shell/",
+                    name: "Home",
+                    image: {
+                      "@type": "ImageObject",
+                      url: "https://localhost/shell/images/head/shell-115x35.20220913070722.jpg",
+                      width: "115",
+                      height: "35",
+                    },
+                  },
+                },
+                {
+                  "@type": "ListItem",
+                  position: 2,
+                  item: {
+                    "@type": "Thing",
+                    "@id": "https://localhost/shell/dummy-1/",
+                    name: "Dummy 1",
+                    image: {
+                      "@type": "ImageObject",
+                      url: "https://localhost/shell/images/head/shell-115x35.20220913070722.jpg",
+                      width: "115",
+                      height: "35",
+                    },
+                  },
+                },
+              ],
+            }),
+          }}
+        />
       </main>
     </>
   );
--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Head from "../components/MicroHead";
+const generatedNonce = process.env.generatedNonce;
 
 export default function Home() {
   return (
@@ -7,10 +8,40 @@ export default function Home() {
         canonical="https://localhost/shell/"
         title="Home | Shell"
         metaDescription="Home page description for SHELL application [70 characters are best here]."
+        nonce={generatedNonce}
       ></Head>
       <main>
         <h1>Home</h1>
         <p>Home page of Shell application</p>
+        <script
+          type="application/ld+json"
+          id="breadcrumb-list"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "BreadcrumbList",
+              description: "Breadcrumbs list",
+              name: "Breadcrumbs",
+              itemListElement: [
+                {
+                  "@type": "ListItem",
+                  position: 1,
+                  item: {
+                    "@type": "Thing",
+                    "@id": "https://localhost/shell/",
+                    name: "Home",
+                    image: {
+                      "@type": "ImageObject",
+                      url: "https://localhost/shell/images/head/shell-115x35.20220913070722.jpg",
+                      width: "115",
+                      height: "35",
+                    },
+                  },
+                },
+              ],
+            }),
+          }}
+        />
       </main>
     </>
   );
